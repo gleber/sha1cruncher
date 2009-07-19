68a32e5c9b6af7710754eba245e82536a65ac1f8
Gleb Peregud <gleber.p@gmail.com>
2009-07-19 19:03:18 +0200

    sha1 executable is distributed with ./scripts/prepare

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -3,7 +3,9 @@
 for i in `cat ~/.hosts.erlang | sed s/%.*//g | sed s/.$//g`; do 
         echo ">> "$i" <<"
         ssh-copy-id -i ~/.ssh/id_rsa.pub $i
-        ssh $i 'chmod 0600 .erlang.cookie'
-        scp .erlang.cookie $i:~
-        ssh $i 'chmod 0600 .erlang.cookie'
+        ssh $i 'chmod 0600 ~/.erlang.cookie'
+        scp ~/.erlang.cookie $i:~
+        ssh $i 'chmod 0600 ~/.erlang.cookie'
+        scp csrc/sha1 $i:/tmp
+        ssh $i 'chmod 0700 /tmp/sha1'
 done
--- src/main.erl
+++ src/main.erl
@@ -41,10 +41,17 @@ init([]) ->
 					    {attributes, record_info(fields, combination)}]),
     ?M("RESULT ~p~n", [Res]),
     pool:start(worker, "-rsh ssh"),
+
     Module = main,
     {_Module, Binary, Filename} = code:get_object_code(Module),
     LoadingCode = rpc:multicall(code, load_binary, [Module, Filename, Binary]),
     ?M("Loading code: ~p~n", [LoadingCode]),
+
+    ?M("Distributing executable... ", []),
+    {Test, []} = rpc:multicall(os, cmd, ["test -f /tmp/sha1 || echo error"]),
+    ?M("Done ~p~n", [Test]),
+    [ "" = X || X <- Test ],
+
     {ok, Dictionary0} = file:read_file("priv/dict.txt"),
     Dictionary = [ list_to_binary(X) || X <- string:tokens(binary_to_list(Dictionary0), " \n") ],
     Workers = lists:foldl(fun(X, D) -> dict:store(X, 0, D) end, dict:new(), pool:get_nodes()),
@@ -76,10 +83,13 @@ handle_cast(_Msg, State) ->
 
 
 handle_info({'EXIT', Pid, normal}, State) ->
-    ?M("pid ~p FINISHED it's work~n", [Pid]),
+    ?M("pid ~p on ~p stopped~n", [Pid, node(Pid)]),
+    {noreply, State};
+handle_info({'EXIT', Pid, more}, State) ->
+    ?M("pid ~p on ~p FINISHED it's work~n", [Pid, node(Pid)]),
     {noreply, remove_worker(Pid, State)};
 handle_info({'EXIT', Pid, Reason}, State) ->
-    ?M("~p EXITed because ~p~n", [Pid, Reason]),
+    ?M("~p on ~p EXITed because ~p~n", [Pid, node(Pid), Reason]),
     {noreply, remove_worker(Pid, State)};
 handle_info(_Info, State) ->
     ?M("UNKNOWN MESSAGE ~p~n", [_Info]),
@@ -106,7 +116,7 @@ better(O, _) ->
 
 add_workers(#state{workers = Workers0, max = Max} = State) ->
     L = lists:keysort(2, dict:to_list(Workers0)),
-    ?M("list ~p~n", [L]),
+    %%?M("list ~p~n", [L]),
     Min = hd(L),
     case Min of
 	{_Node, Max} ->
@@ -144,27 +154,33 @@ worker_loop0(MasterNode) ->
     worker_loop(MasterNode).
 
 worker_loop(MasterNode) ->
-    try
-	?W("will send a request for job~n", []),
-	{ok, Combination} = gen_server:call({?SERVER, MasterNode}, {get, self()}),
-	Port = open_port({spawn, "csrc/sha1"}, [{line, 1000},
-						use_stdio]),
-	port_command(Port, [Combination, "\n"]),
-	?W("waiting for job to finish~n", []),
-	Line = receive
-		   {Port, {data, {eol, L}}} ->
-		       ?W("Got ~p~n", [L]),
-		       L;
-		   _X -> 
-		       ?W("Received ~p~n", [_X]),
-		       exit(badmsg)
-	       after
-		   10000 ->
-		       exit(timeout)
-	       end,					     
-	[Phrase, Score] = string:tokens(Line, "\t"),
-	gen_server:cast({?SERVER, MasterNode}, {done, self(), Combination, {Phrase, list_to_integer(Score)}})
-    catch _E:_A ->
-	    ?W("Error! ~p:~p~n~p~n", [_E, _A, erlang:get_stacktrace()])
-    end.
+
+    ?W("will send a request for job~n", []),
+    {ok, Combination} = gen_server:call({?SERVER, MasterNode}, {get, self()}),
+    Port = open_port({spawn, "/tmp/sha1"}, [{line, 1000},
+					    use_stdio]),
+%%     receive
+%% 	{'EXIT', Port, Error} ->
+%% 	    ?W("Port is dead ~p~n", [Error]),
+%% 	    exit(Error)
+%%     after
+%% 	200 ->
+%% 	    ok
+%%     end,
+    port_command(Port, [Combination, "\n"]),
+    ?W("waiting for job to finish~n", []),
+    Line = receive
+	       {Port, {data, {eol, L}}} ->
+		   ?W("Got ~p~n", [L]),
+		   L;
+	       _X -> 
+		   ?W("Received ~p~n", [_X]),
+		   exit(badmsg)
+	   after
+	       10000 ->
+		   exit(timeout)
+	   end,					     
+    [Phrase, Score] = string:tokens(Line, "\t"),
+    gen_server:cast({?SERVER, MasterNode}, {done, self(), Combination, {Phrase, list_to_integer(Score)}}),
+    exit(more).
 
